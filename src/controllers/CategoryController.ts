845917eebbac9732c392d33c715eaf524b8fb7b8
jessicavsampaio <8433012+jessicavsampaio@users.noreply.github.com>
2023-11-18 08:56:31 -0300

    criação dos controllers de update e delete e as routes de Category

--- src/controllers/CategoryController.ts
+++ src/controllers/CategoryController.ts
@@ -29,3 +29,39 @@ export class ListCategoryController{
     }
   }
 }
+
+export class updateCategoryController {
+  async handle(request: Request, response: Response){
+    try{
+      const { name } = request.body;
+      const id = request.params.id;
+      
+      const updateCategory = await prismaClient.category.update({
+        where: { id },
+        data: {
+          name
+        }
+      });
+
+      return response.status(200).json({message:'Category updated ', updateCategory});
+
+    }catch(error){
+      return response.status(500).json({messageError:'Server Error!'})
+    }
+  }
+}
+
+export class deleteCategoryController{
+  async handle(request: Request, response: Response){
+    try{
+      const id = request.params.id;
+      const deleteCategory = await prismaClient.category.delete({
+        where:{id}
+      });
+      return response.status(204).json({});
+    }
+    catch(error){
+      return response.status(500).json({messageError:'Server Error!'});
+    }
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { createEventController, deleteController, listEventController, updateController } from "./controllers/CreateEventController";
-import { CreateCategoryController, ListCategoryController } from "./controllers/CategoryController";
+import { CreateCategoryController, ListCategoryController, deleteCategoryController, updateCategoryController } from "./controllers/CategoryController";
 import { CreatePlaceController, ListPlaceController } from "./controllers/PlaceController";
 
 const router = Router();
@@ -9,7 +9,6 @@ const createEvent = new createEventController();
 router.post("/event", createEvent.handle);
 const listEvent = new listEventController();
 router.get("/event", listEvent.handle);
-
 const updateList = new updateController();
 router.put("/event", updateList.handle)
 const delete_id = new deleteController();
@@ -18,7 +17,11 @@ router.delete("/event", delete_id.handle)
 const createCategory = new CreateCategoryController();
 router.post("/category", createCategory.handle);
 const listCategory = new ListCategoryController();
-router.get("/listCategory", listCategory.handle);
+router.get("/category", listCategory.handle);
+const updateCategory = new updateCategoryController();
+router.put("/category", updateCategory.handle)
+const deleteCategory = new deleteCategoryController();
+router.delete("/category", deleteCategory.handle)
 
 const createPlace = new CreatePlaceController();
 router.post("/place", createPlace.handle);
